93abb2fc4ede93ba2b809305d84d2229a19f466b
1san999 <78712261+1san999@users.noreply.github.com>
2022-05-31 17:41:34 -0700

    Add files via upload

---
+++ object-detection/main.js
@@ -0,0 +1,37 @@
+img = "";
+status1="";
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectdetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="status detecting objects";
+}
+function modelLoaded(){
+console.log("model is loaded");
+status1=true;
+objectdetector.detect(img, gotResult)
+}
+function preload(){
+    img=loadImage("dog_cat.jpg")
+}
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else
+    console.log(results);
+}
+function draw(){
+    image(img, 0, 0, 640,420);
+    fill("#ff0000");
+    text("Dog", 45, 75);
+    noFill()
+    stroke("#ff0000")
+    rect(30, 60, 450, 350);
+    
+    fill("#ff0000");
+    text("Cat", 320, 120);
+    noFill()
+    stroke("#ff0000")
+    rect(300, 40,270, 320);
+}
